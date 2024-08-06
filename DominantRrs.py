73f2669fcde5a8ed552efab7c0d64bf61b72b268
kittykam <kitty.kam@dal.ca>
2024-08-06 17:57:48 +0000

    Complete Rrs average function
    add processing and plotting within the function

--- DominantRrs.py
+++ DominantRrs.py
@@ -9,12 +9,37 @@ from random import randint
 auth = earthaccess.login(persist=True)
 
 
-def Rrs_avg(flag, lat_N, lat_S, lon_W, lon_E):
-    tspan = ("2024-07-19", "2024-08-03")
-    results = earthaccess.search_data(
-        short_name="PACE_OCI_L3M_RRS_NRT",
-        temporal=tspan,
-        granule_name="*.8D.*.0p1deg.*",
-    )
+# function: make sure dataset and flag have the same dimension on lat and lon
 
-    paths = earthaccess.open(results)
+def Rrs_avg(dataset,flag, n):
+    # dataset = 3D (not nested)
+    # flag = 2D array flag
+    # n = desired flag
+    
+    # add flag into the dataset
+    dataset['flag'] = (('lat','lon'),flag)
+    
+    # pull lat lon index from flag
+    lat_idx = np.where(flag == n)[0]
+    lon_idx = np.where(flag == n)[1]
+    
+    # pull the Rrs from the selected location
+    Rrs = dataset.Rrs.values[lat_idx, lon_idx,:]
+    wv = dataset.wavelength
+
+    # calculate mean and sd
+    Rrs_mean = np.nanmean(Rrs,axis = 0)
+    Rrs_sd = np.nanstd(Rrs, axis = 0)
+
+    # return 
+    return Rrs_mean, Rrs_sd
+
+    #plot 
+    fig, ax = plt.subplots(1)
+    ax.plot(wv, Rrs_mean, lw=2, color='blue')
+    ax.fill_between(wv, Rrs_mean+Rrs_sd, Rrs_mean-Rrs_sd, facecolor='blue', alpha=0.5)
+    ax.set_title(r'Rrs spectral average : {} to {}'.format(datetime.fromisoformat(dataset.attrs['time_coverage_start'][:-1]).astimezone(timezone.utc).strftime("%Y-%m-%d"),
+                                                           datetime.fromisoformat(dataset.attrs['time_coverage_end'][:-1]).astimezone(timezone.utc).strftime("%Y-%m-%d")))
+    ax.set_xlabel('Wavelength (nm)')
+    ax.set_ylabel('$R_{rs} \ (m^{-1})$')
+    ax.grid()
